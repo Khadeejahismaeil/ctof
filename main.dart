67c7e5182b0e22da47b374a3fc83295e55a85b9a
Khadeejahismaeil <skhadijahalalawy@gmail.com>
2024-09-30 09:56:22 +0300

    first commit

---
+++ main.dart
@@ -0,0 +1,14 @@
+void main() {
+  double tempInFahrenheit = 86;
+  double celsius = tempInFahrenheit - 32 / 1.8;
+  print("Fahrenheit($tempInFahrenheit F) to Celsius: $celsius°C");
+}
+/*String name = "khadeejah";
+  int age = 23;
+  double hight = 1.63;
+   bool loveCoding = true;
+  print(
+      "my name is $name and im $hight tall and if youre wondring if i love coding then thats $loveCoding");
+  print("my age is ${age + 1}");
+}
+{*/
